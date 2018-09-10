8a335ba3b6a3c6fdacef9acf7858c9fb448ceb77
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-09-10 14:23:47 -0400

    index-update-22 / style-update-35 / script-update-4
    initial addition of swapImages() and crossFade() functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 /* ------------------------ IMAGE FILE DECLARATIONS ------------------------ */
+var testimonialsVideoAnn = "https://res.cloudinary.com/dtwyohvli/image/upload/v1536416417/nsm-mentor-landing/testimonials-video-ann.png";
+var testimonialsVideoStudent = "https://res.cloudinary.com/dtwyohvli/image/upload/v1536416417/nsm-mentor-landing/testimonials-video-student.png";
+
 var basicPlanImg = "https://res.cloudinary.com/dtwyohvli/image/upload/v1536461113/nsm-mentor-landing/price-table-basic-plan.png";
 var advancedPlanImg = "https://res.cloudinary.com/dtwyohvli/image/upload/v1536461113/nsm-mentor-landing/price-table-advanced-plan.png";
 var deluxePlanImg = "https://res.cloudinary.com/dtwyohvli/image/upload/v1536461113/nsm-mentor-landing/price-table-deluxe-plan.png";
@@ -18,11 +21,54 @@ $(document).ready(function() {
 
   var $testimonialsArrowLeft = $("#testimonials-arrow-left");
   var $testimonialsArrowRight = $("#testimonials-arrow-right");
-  var $testimonialsVideoAnn = $("#testimonials-video-ann");
+  var $testimonialsVideoMain = $("#testimonials-video-main");
+  var $videoMainGhost = $("#video-main-ghost");
+  var $testimonialsVideoSub = $("#testimonials-video-sub");
+  var $videoSubGhost = $("#video-sub-ghost");
 
   var $priceTableBasicPlan = $("#price-table-basic-plan");
   var $priceTableAdvancedPlan = $("#price-table-advanced-plan");
   var $priceTableDeluxePlan = $("#price-table-deluxe-plan");
+
+
+/* ------------------------- Function Declarations ------------------------- */
+
+  function swapImages() {
+    if($testimonialsVideoMain.attr("src") === testimonialsVideoAnn) {
+      $testimonialsVideoMain.attr("src", testimonialsVideoStudent);
+      $testimonialsVideoSub.attr("src", testimonialsVideoAnn);
+
+      $videoMainGhost.attr("src", testimonialsVideoAnn);
+      $videoSubGhost.attr("src", testimonialsVideoStudent);
+    }
+    else if($testimonialsVideoMain.attr("src") === testimonialsVideoStudent) {
+      $testimonialsVideoMain.attr("src", testimonialsVideoAnn);
+      $testimonialsVideoSub.attr("src", testimonialsVideoStudent);
+
+      $videoMainGhost.attr("src", testimonialsVideoStudent);
+      $videoSubGhost.attr("src", testimonialsVideoAnn);
+    }
+  }
+
+
+  function crossFadeMain() {
+    $videoMainGhost.removeClass("fade-out");
+    $videoMainGhost.css("opacity", "10");
+
+    setTimeout(function() {
+      $videoMainGhost.addClass("fade-out");
+    }, 0);
+  }
+
+
+  function crossFadeSub() {
+    $videoSubGhost.removeClass("fade-out");
+    $videoSubGhost.css("opacity", "10");
+
+    setTimeout(function() {
+      $videoSubGhost.addClass("fade-out");
+    }, 0);
+  }
 
 
 /* ---------------------------- EVENT HANDLERS ---------------------------- */
@@ -50,6 +96,12 @@ $(document).ready(function() {
   $testimonialsArrowLeft.mouseleave(function() {
     $(this).removeClass("testimonials-arrow-hover-left");
   });
+  $testimonialsArrowLeft.on("click", function() {
+    swapImages();
+
+    crossFadeMain();
+    crossFadeSub();
+  });
 
   $testimonialsArrowRight.mouseenter(function() {
     $(this).addClass("testimonials-arrow-hover-right");
@@ -57,12 +109,18 @@ $(document).ready(function() {
   $testimonialsArrowRight.mouseleave(function() {
     $(this).removeClass("testimonials-arrow-hover-right");
   });
+  $testimonialsArrowRight.on("click", function() {
+    swapImages();
 
-  $testimonialsVideoAnn.mouseenter(function() {
-    $(this).addClass("testimonials-video-hover");
+    crossFadeMain();
+    crossFadeSub();
   });
-  $testimonialsVideoAnn.mouseleave(function() {
-    $(this).removeClass("testimonials-video-hover");
+
+  $videoMainGhost.mouseenter(function() {
+    $testimonialsVideoMain.addClass("testimonials-video-hover");
+  });
+  $videoMainGhost.mouseleave(function() {
+    $testimonialsVideoMain.removeClass("testimonials-video-hover");
   });
 
 
